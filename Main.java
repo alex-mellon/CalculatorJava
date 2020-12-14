ce17106372301fc3e07ba61e80a64cbe13969f14
Александр <64964177+alex-mellon@users.noreply.github.com>
2020-12-14 15:43:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ package com.company;
 public class Main {
 
 
-    public static void main(String[] args) throws Error {
+    public static void main(String[] args) throws MyError {
         System.out.print("Enter numbers(between 1 and 10 inclusive)  and operation: ");
 
         String number1 = Operations.getNumb();
         char operation = Operations.getOperation();
         String number2 = Operations.getNumb();
-        System.out.println( JustToIntAndBack.JustToIntAndBack(number1, number2, operation) );
+        String ans = JustToIntAndBack.JustToIntAndBack(number1, number2, operation);
+        System.out.println(ans);
 
     }
 
